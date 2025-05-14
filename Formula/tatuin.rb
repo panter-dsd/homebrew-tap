b3d45b5a152372469ea0bcabafa5f0d2f6cc60be
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-05-14 22:48:52 +0300

    Fix description.

--- Formula/tatuin.rb
+++ Formula/tatuin.rb
@@ -1,5 +1,5 @@
 class Tatuin < Formula
-  desc "Tatuin (Task Aggregator TUI for N providers)"
+  desc "Task Aggregator TUI for N providers"
   homepage "https://github.com/panter-dsd/tatuin"
   url "https://github.com/panter-dsd/tatuin/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "b7761a3617f6657b91760479932dad5305e6069caaa1fdf6f1970822ef6bf5e1"

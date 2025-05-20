523f9c1e51fce7519364e81f8cb6d248735401ab
Konstantin Konnov <konstantin.konnov@panter-dsd.ru>
2025-05-20 21:53:32 +0300

    Up version to v0.10.0

--- Formula/tatuin.rb
+++ Formula/tatuin.rb
@@ -1,8 +1,8 @@
 class Tatuin < Formula
   desc "Task Aggregator TUI for N providers"
   homepage "https://github.com/panter-dsd/tatuin"
-  url "https://github.com/panter-dsd/tatuin/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "d7c81a1bd769f85204fbe4657aafa8ea368c6ade7284d1dd4be4c305079b4e4b"
+  url "https://github.com/panter-dsd/tatuin/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "5cbe8f6103dbbdd541019f53f27ddf63c3ee5dcbb7a6c743993793a5efdbced6"
   license "MIT"
 
   depends_on "rust" => :build
